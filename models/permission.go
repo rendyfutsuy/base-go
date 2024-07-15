ac6f3c76495145decccaa1790f9a21ec59f82347
rendyfutsuy <rendy.anggara@roketin.com>
2024-07-15 13:31:40 +0700

    add permission

---
+++ models/permission.go
@@ -0,0 +1,13 @@
+package models
+
+import "github.com/google/uuid"
+
+// Permission represent the role model
+type Permission struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name" validate:"required"`
+	Deletable bool      `json:"deletable"`
+	TotalUser *int      `json:"total_user"`
+
+	Users []User `json:"users"`
+}
