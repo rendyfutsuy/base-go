aecbd488232d20d745dcf6a7b83656fec7c12d04
rendyfutsuy <rendy.anggara@roketin.com>
2025-10-13 13:35:32 +0700

    add seeders

--- database/seeders/000002_seed_users.up.sql
+++ database/seeders/000002_seed_users.up.sql
@@ -23,7 +23,7 @@ INSERT INTO users (
     NOW(),
     NOW(),
     NOW() + INTERVAL '90 days',
-    'admin@mailinator.com',
+    'superuser@mailinator.com',
     'admin',
     '$2y$10$GViZu3GONfwoswHMagB0sOh.ZlKeK9WrSyhwbvmiheeGGihz2vBSm',
     'a43a5e5f-a172-42d1-a70e-8834bf653eb0',
@@ -32,44 +32,4 @@ INSERT INTO users (
     'male',
     0,
     '3175091206980001'
-),
-(
-    -- Super Admin User
-    -- password: password123
-    -- role: Super Admin
-    -- password expired at: now - 2 days
-    -- status active
-    uuid_generate_v7(),
-    NOW(),
-    NOW(),
-    NOW() - INTERVAL '2 days',
-    'admin.password.expired@mailinator.com',
-    'admin.password.expired',
-    '$2y$10$GViZu3GONfwoswHMagB0sOh.ZlKeK9WrSyhwbvmiheeGGihz2vBSm',
-    'a43a5e5f-a172-42d1-a70e-8834bf653eb0',
-    'Admin - Password Expired',
-    TRUE,
-    'male',
-    0,
-    '3276012501800002'
-),
-(
-    -- Super ADmin User
-    -- password: password123
-    -- role: Super Admin
-    -- password expired at: now + 90 days
-    -- status inactive
-    uuid_generate_v7(),
-    NOW(),
-    NOW(),
-    NOW() + INTERVAL '90 days',
-    'admin.inactive@mailinator.com',
-    'admin.inactive',
-    '$2y$10$GViZu3GONfwoswHMagB0sOh.ZlKeK9WrSyhwbvmiheeGGihz2vBSm',
-    'a43a5e5f-a172-42d1-a70e-8834bf653eb0',
-    'Admin - Inactive',
-    FALSE,
-    'male',
-    0,
-    '3374121301900003'
-);
+)
