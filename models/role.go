2257cf0b355af0c084fc8b06c2b4ab2e721ad6e0
rendyfutsuy <rendy.anggara@roketin.com>
2025-10-31 09:01:43 +0700

    integrate gorm to role_management models

--- models/role.go
+++ models/role.go
@@ -9,21 +9,28 @@ import (
 
 // Role represent the role model
 type Role struct {
-	ID                   uuid.UUID          `json:"id"`
-	Name                 string             `json:"name" validate:"required"`
-	Deletable            bool               `json:"deletable"`
-	TotalUser            int                `json:"total_user"`
-	Modules              []utils.NullString `json:"modules"`
-	CategoryNames        []utils.NullString `json:"category_names"`
-	CreatedAt            time.Time          `json:"created_at"`
-	UpdatedAt            utils.NullTime     `json:"updated_at"`
-	DeletedAt            utils.NullTime     `json:"deleted_at"`
-	PermissionGroupNames []utils.NullString `json:"permission_group_names"`
-	PermissionGroupIds   []uuid.UUID        `json:"permission_group_ids"`
-	Description          utils.NullString   `json:"description"`
+	ID          uuid.UUID        `gorm:"type:uuid;primary_key;default:uuid_generate_v7()" json:"id"`
+	Name        string           `gorm:"type:varchar(255);not null;uniqueIndex" json:"name" validate:"required"`
+	Deletable   bool             `gorm:"column:deletable;default:true;not null" json:"deletable"`
+	CreatedAt   time.Time        `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt   utils.NullTime   `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt   utils.NullTime   `gorm:"column:deleted_at;index" json:"deleted_at"`
+	Description utils.NullString `gorm:"column:description;type:text" json:"description"`
 
-	// relation
-	Users            []User            `json:"users"`
-	Permissions      []Permission      `json:"permissions"`
-	PermissionGroups []PermissionGroup `json:"permission_groups"`
+	// Computed/virtual fields - not stored in DB
+	TotalUser            int                `gorm:"-" json:"total_user"`
+	Modules              []utils.NullString `gorm:"-" json:"modules"`
+	CategoryNames        []utils.NullString `gorm:"-" json:"category_names"`
+	PermissionGroupNames []utils.NullString `gorm:"-" json:"permission_group_names"`
+	PermissionGroupIds   []uuid.UUID        `gorm:"-" json:"permission_group_ids"`
+
+	// Relations
+	Users            []User            `gorm:"foreignKey:RoleId" json:"users"`
+	Permissions      []Permission      `gorm:"many2many:permissions_modules;" json:"permissions"`
+	PermissionGroups []PermissionGroup `gorm:"many2many:modules_roles;" json:"permission_groups"`
+}
+
+// TableName specifies table name for GORM
+func (Role) TableName() string {
+	return "roles"
 }
